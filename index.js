199ce2304c801fe23fad9a2c041f21b16ba86145
Olaf Peters <olaf@guesswhapp.de>
2015-01-21 22:31:56 +0100

    Add Newsletter site with input Form

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
-	.create({ defaultLayout:'main' });	
+	.create({
+		defaultLayout:'main',
+		helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});	
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set('port', process.env.PORT || 3000);
 
 //Routes
 app.get('/', function(req, res) {
 	res.render('home');
+});
+
+app.get('/thank-you', function(req, res){
+	res.render('thank-you');
+});
+
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
 });
 
 // 404 catch-all handler (middleware)
