0a0e9764ef3258a55437b78994b7a95ba8bbba3c
Olaf Peters <olaf@guesswhapp.de>
2015-01-30 21:35:09 +0100

    Added Product Overview with link to single Product Page and Add-to-Cart

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,8 @@ module.exports = {
 	registerRoutes: function(app) {
 		app.get('/products/create', this.create);
 		app.post('/products/create', this.productCreation);
-		app.get('/products/:nr', this.home);
+		app.get('/products', this.productsOverview);
+		app.get('/products/:nr', this.productSingle);
 	},
 
 	create: function(req, res, next) {
@@ -21,20 +22,34 @@ module.exports = {
 		});
 		c.save(function(err) {
 			if(err) return next(err);
-			res.redirect(303, '/products/' + c.nr);
+			res.redirect(303, '/products');
 		});
 	},
 
-	home: function(req, res, next) {
-
+	productSingle: function(req, res, next) {
 		Products.find({ nr : req.params.nr }, function(err, product) {
 			if(err) return res.redirect(303, '/error');
 			if(!product) return next(); 	// pass this on to 404 handler
-			res.render('products/home', {
+			res.render('products/single', {
 				productName: product[0].productName,
 				nr: product[0].nr,
 				price: product[0].price,
 			});
 		});
+	},
+
+	productsOverview: function (req, res, next) {
+		Products.find(function(err, products) {
+			var context = {
+				products: products.map(function(product){
+					return {
+						nr: product.nr,
+						productName: product.productName,
+						price: product.price,
+					}
+				})
+			};
+			res.render('products/overview', context);		
+		})
 	},
 };
