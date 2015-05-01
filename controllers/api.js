278c54380eb85a0e70ca4790e7344e8d1067eeb3
Olaf Peters <olaf@guesswhapp.de>
2015-05-01 10:05:55 +0200

    Reprogrammed API to redeem Codes

--- controllers/api.js
+++ controllers/api.js
@@ -9,77 +9,6 @@ function publish(context, statusCode, req, res){
     res.status(statusCode).json(context);
 }; // pubilsh
 
-function markEachCode(reelId, indexEditedCodes, APIUser) {
-    Reels.findById(reelId, function(err, reel){
-        reel.codes.forEach(function(codesInReel){
-            if (indexEditedCodes != 0 && codesInReel.cStatus == 1 && JSON.stringify(APIUser)==JSON.stringify(codesInReel.consumer)) {
-                indexEditedCodes--; // mark only required number of codes to be redeemed (therefore the index)
-                codesInReel.cStatus = 2; // Edit status of Code to "redeem = 2"
-            }
-        }); // codes.forEach
-        reel.save(function(err) {
-            if (err) { return next(err); }
-        });
-    }); // Reels findById
-    return indexEditedCodes;
-};
-
-function updateUserStats(APIUser, programId, programNr, callback){
-    var correctUser = false;
-    CUsers.findById(APIUser, function(err, cUser){
-        // Update HitGoalStats of User (decrease)
-        cUser.hitGoalPrograms.forEach(function(progHitGoal){
-            if (JSON.stringify(progHitGoal.program)==JSON.stringify(programId)){
-                correctUser = true;
-                progHitGoal.hitGoalCount--; // HitGoal Stats decrease (since codes are beeing redeemed)
-            } // found redeemed Program
-        }); // hitGoalProgram.forEach
-        // Update RedeemStats of User (Increase)
-        var firstTime = true; // marks if this User has redeemd this program for the 1st time
-        cUser.redeemPrograms.forEach(function(redeemProgram){
-            if(JSON.stringify(redeemProgram.program)==JSON.stringify(programId)){
-                redeemProgram.redeemCount++;
-                firstTime= false; // yes, User did already redeem this Program once before (only increase counter)
-            } // if User already has redeemd in the program
-        }); // forEach redeemProgram
-        if (firstTime==true){ // it is the 1st time
-            var programArray = {
-                program: programId,
-                redeemCount: 1
-            };
-            cUser.redeemPrograms.push(programArray);
-        } // it is the 1st time
-        cUser.save(function(err){
-            if (err) { return next(err);}
-        }); // save updated User Stats
-        if (correctUser) {
-            callback(programNr);
-        } else {
-            return false;
-        }
-        return;
-    }); // CUsers findById
-}; // updateUserStats
-
-function updateProgramStats(programNr){
-    var programArray = [];
-    Programs.findOne({'nr' : programNr}, function(err, updateProgram){
-        updateProgram.hitGoalsCount--;
-        updateProgram.redeemCount++;
-        programArray = {
-            programName: updateProgram.programName,
-            hitGoalsCount: updateProgram.hitGoalsCount,
-            redeemCount: updateProgram.redeemCount,
-        };
-        updateProgram.save(function(err){
-            if (err) { return next(err);}
-        }); // save updated User Stats
-        console.log('vor rückehr vom Callback');
-        console.log(programArray);
-        return programArray;
-    }); // Program FindOne
-}; // updateProgramStats
-
 function checkUser(req, res, next) {
 	console.log(req.body);
     // User identifizieren
@@ -214,48 +143,166 @@ module.exports = {
 	}, // Function processApiCodeScan
 
     processApiCodeRedeem: function (req, res, next){
-        APIUser = res.locals.apiuser;
-        var indexEditedCodes = req.body.programGoal;
-        var reelIdArray = [];
+        // APIUser = res.locals.apiuser; var indexEditedCodes = req.body.programGoal; var reelIdArray = [];
         Programs.findOne({'nr' : req.body.programNr}, function(err, programs){
-            reelIdArray = programs.allocatedReels.map(function(allocatedReel){
-                return allocatedReel;
-            }); // map programs
-            reelIdArray.forEach(function(reelId, index){
-                if (indexEditedCodes != 0) {
-                    indexEditedCodes = markEachCode(reelId, indexEditedCodes, APIUser);
-                } // if counter 1= 0
-                if (index == reelIdArray.length-1){ // perform next Step (in sync) after ForEach Index complete
-                    updateUserStats(APIUser, programs._id, req.body.programNr, updateProgramStats)
-                    console.log('bis hier zurück?');
-                    context = {
-                        success: true,
-                        message : "Herzliche Glückwünsche, Punkte wurden eingelöst",
-                        reels: reelIdArray,
-                    }; // context
-                    statusCode = 200;
-                    publish(context, statusCode, req, res);
-                }
-            }); // allocReels.forEach
+            if (err || !programs || !programs.allocatedReels){ // if Program ist nicht vorhanden
+                console.log(err);
+                console.log(programs);
+                context = {
+                    success: false,
+                    programName: "Program Unbekannt",
+                    message : "Auf diesem Program darf es keine zu sammelnde Punkte geben",
+                }; // context
+                statusCode = 400;
+                publish(context, statusCode, req, res);
+                return;
+            } else if (req.body.programGoal != programs.goalToHit) {
+                context = {
+                    success: false,
+                    programName: programs.programName,
+                    requestGoalToHit: req.body.programGoal,
+                    programGoalToHit: programs.goalToHit,
+                    message : "Bitte sprechen Sie den Ladeninhaber an welches die gezielten Zielpunkte sind",
+                }; // context
+                statusCode = 400;
+                publish(context, statusCode, req, res);
+                return;
+            } else { // if Program ist nicht vorhanden oder DOCH vorhanden (=normal)
+                checkCodesInReels(programs._id, req, res);
+            } // if Program ist doch vorhanden (kein err in Programs find)
         }); // Programs find
-
-                // context = {
-                //     success: false,
-                //     programName: programs.programName,
-                //     message : req.body.user + ' hat noch nicht genug Punkt für Einlösung gesammelt',
-                // }; // context
-                // statusCode = 400;
-                // publish(context, statusCode, req, res);
-
-                // context = {
-                //     success: false,
-                //     programName: programs.programName,
-                //     message : "Wir konnten nicht genug dem User und Program zugeordnete & eingelöst Punkte finden",
-                //     reels: allocReels,
-                // }; // context
-                // statusCode = 400;
-                // publish(context, statusCode, req, res);
-
     }, // Function processApiCodeRedeem
-
 };
+
+function checkCodesInReels (programId, req, res){
+    var codesArray = [];
+    var counter =  req.body.programGoal;
+    var helpIndex = 0;
+    Reels
+            .find({'assignedProgram' : programId })
+            .select('_id codes.rCode codes.cStatus codes.consumer')
+            .exec(function(err, reels){
+        reels.forEach(function(reel, index){
+            var reelId = reel._id;
+            reel.codes.forEach(function(reelcode){
+                if (reelcode.cStatus==1 && counter!= 0 && JSON.stringify(reelcode.consumer) == JSON.stringify(res.locals.apiuser)){
+                    var codesArrayIndex = Object.keys(codesArray).length;
+                    if (codesArrayIndex == 0 || Object.keys(codesArray)[codesArrayIndex - 1] != reelId){
+                        codesArray[reelId] = [{ rCode: reelcode.rCode}];
+                    } else {
+                        var objArray = { rCode: reelcode.rCode};
+                        codesArray[reelId].push(objArray);
+                    }
+                    counter--;
+                } //  if cStatus = 1
+            }); // reels.forEach
+            helpIndex = index;
+        }); // reels.forEach
+        if (helpIndex == reels.length-1){
+            if (counter != 0){
+                context = {
+                    success: false,
+                    programNr: req.body.programNr,
+                    message : "Für dieses Program liegen nicht genug Punkte vor.",
+                }; // context
+                statusCode = 400;
+                publish(context, statusCode, req, res);
+                return;
+            } else {
+                console.log('super!');
+                updateUserStats(programId, codesArray, req, res);
+            }
+        } // wir sind am Ende
+    }); // Reels.find
+}; // checkCodesInReels
+
+function updateUserStats(programId, codesArray, req, res){
+    var correctUser = false;
+    var didRun = false;
+    CUsers.findById(res.locals.apiuser, 'hitGoalPrograms redeemPrograms', function(err, cUser){
+        console.log(cUser);
+        // Update HitGoalStats of User (decrease)
+        cUser.hitGoalPrograms.forEach(function(progHitGoal){
+            if (JSON.stringify(progHitGoal.program)==JSON.stringify(programId)){
+                correctUser = true;
+                progHitGoal.hitGoalCount--; // HitGoal Stats decrease (since codes are beeing redeemed)
+            } // found redeemed Program
+        }); // hitGoalProgram.forEach
+        // Update RedeemStats of User (Increase)
+        var firstTime = true; // marks if this User has redeemd this program for the 1st time
+        cUser.redeemPrograms.forEach(function(redeemProgram){
+            if(JSON.stringify(redeemProgram.program)==JSON.stringify(programId)){
+                redeemProgram.redeemCount++;
+                firstTime= false; // yes, User did already redeem this Program once before (only increase counter)
+            } // if User already has redeemd in the program
+        }); // forEach redeemProgram
+        if (firstTime==true){ // it is the 1st time
+            var programArray = {
+                program: programId,
+                redeemCount: 1
+            };
+            cUser.redeemPrograms.push(programArray);
+        } // it is the 1st time
+        cUser.save(function(err){
+            if (err) { return next(err);}
+        }); // save updated User Stats
+        if (correctUser) {
+            console.log(cUser);
+            if (didRun==false){updateProgramStats(codesArray, req, res);}
+            didRun=true;
+        }
+        return;
+    }); // CUsers findById
+}; // updateUserStats
+
+function updateProgramStats(codesArray, req, res){
+    Programs.findOne({'nr' : req.body.programNr}, function(err, updateProgram){
+        updateProgram.hitGoalsCount--;
+        updateProgram.redeemCount++;
+        updateProgram.save(function(err){
+            if (err) { return next(err);}
+        }); // save updated User Stats
+        context = {
+            success: true,
+            message : "Herzliche Glückwünsche, Punkte wurden eingelöst",
+            programName: updateProgram.programName,
+            hitGoalsCount: updateProgram.hitGoalsCount,
+            redeemCount: updateProgram.redeemCount,
+        }; // context
+        statusCode = 200;
+        publish(context, statusCode, req, res)
+        console.log('und jetzt geht es zu markCodesInReel');
+        markCodesInReel(codesArray, req, res);
+        return;
+    }); // Program FindOne
+}; // updateProgramStats
+
+function markCodesInReel(codesArray, req, res) {
+    for (var i = 0; i<Object.keys(codesArray).length; i++){
+        var reelId =Object.keys(codesArray)[i]; 
+        var reelArray = codesArray[reelId];
+        markEachCode(reelId, reelArray);
+    } // for i (number of reels in codesArray)
+}; // markEachCode
+
+function markEachCode(reelId, reelArray) {
+    var changedReel = false;
+    Reels.findById(reelId, 'codes', function(err, reels){
+        var codeIndex = 0;
+        reels.codes.forEach(function(reel){
+            if (codeIndex<reelArray.length) {
+                if (reel.rCode == reelArray[codeIndex].rCode) {
+                    reel.cStatus = 2;
+                    changedReel = true;
+                    codeIndex++;
+                    console.log('found ' + reel.rCode);
+                } // found rCode
+            } // if codeIndex within reelArray Index
+        }); // forEach reels.code
+        if (changedReel=true) {
+            reels.save(function(err){
+                if (err) { return next(err);}
+            }); // save updated User Stats
+        } // changedReel = true
+    }); // Reels.findById
+}; // markEachCode
